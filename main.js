083bc175bb3f78e097a8e2bd121b3ca608842c42
VictorHOG95 <victorhog95@gmail.com>
2022-09-13 01:56:16 -0500

    Se añadio funcionalidad en la seccion de esperiencia que permite cambiar entre la información de la educación y la información del trabajo.

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+/* Experience Tabs */
+const tabs = document.querySelectorAll('[data-target]'),
+        tabContents = document.querySelectorAll('[data-content]');
 
+tabs.forEach(tab=>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target);
 
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('experience-active');
+        });
+        target.classList.add('experience-active');
+
+        tab.forEach(tab =>{
+            tab.classList.remove('experience-active');
+        });
+        tab.classList.add('experience-active');
+    });
+});
+/*  */
 function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     //
