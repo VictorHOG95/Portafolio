579265324b7fd9fe0e02836bcc358b50d87f2822
VictorHOG95 <victorhog95@gmail.com>
2022-09-19 23:16:46 -0500

    Se creo un menu de hamburguesa deslizable.

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 /* Experience Tabs */
 const tabs = document.querySelectorAll('[data-target]'),
-        tabContents = document.querySelectorAll('[data-content]');
+    tabContents = document.querySelectorAll('[data-content]');
 
-tabs.forEach(tab=>{
-    tab.addEventListener('click', () =>{
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target);
 
-        tabContents.forEach(tabContent =>{
+        tabContents.forEach(tabContent => {
             tabContent.classList.remove('experience-active');
         });
         target.classList.add('experience-active');
 
-        tab.forEach(tab =>{
+        tab.forEach(tab => {
             tab.classList.remove('experience-active');
         });
         tab.classList.add('experience-active');
@@ -29,3 +29,12 @@ function scrollUp() {
 }
 
 window.addEventListener('scroll', scrollUp);
+
+/**/
+const menu_btn_hamburger = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+
+menu_btn_hamburger.addEventListener('click', function () {
+    menu_btn_hamburger.classList.toggle('is-active');
+    mobile_menu.classList.toggle('active');
+});
