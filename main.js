05059325978b0b155357e4c0dd7aad4e9214b3c1
VictorHOG95 <victorhog95@gmail.com>
2022-09-21 00:57:07 -0500

    Creación del menu de navegación para dispositivos de tamaño pantalla mayor a 768px. el menu esta anclado a la parte superior y tiene un logo centrado. Al dar scroll a la pagina se agrega un color de fondo al menu y una sombra.

--- main.js
+++ main.js
@@ -33,8 +33,21 @@ window.addEventListener('scroll', scrollUp);
 /**/
 const menu_btn_hamburger = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header.container');
 
 menu_btn_hamburger.addEventListener('click', function () {
     menu_btn_hamburger.classList.toggle('is-active');
     mobile_menu.classList.toggle('active');
+});
+
+/**/
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#d2d8d8';
+        header.style.boxShadow = '0px 5px 10px 0px rgba(0, 0, 0, 0.3)';
+    } else {
+        header.style.backgroundColor = 'transparent';
+        header.style.boxShadow = 'none';
+    }
 });
